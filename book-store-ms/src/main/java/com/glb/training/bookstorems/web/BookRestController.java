b32d2cf9c85b5bc01ad2af5becf3aa3b77c74f13
Luis Cortes Coy <lfcortes12@gmail.com>
2019-07-18 16:05:49 -0500

    enables refresh scope by using spring actuator endpoint

--- book-store-ms/src/main/java/com/glb/training/bookstorems/web/BookRestController.java
+++ book-store-ms/src/main/java/com/glb/training/bookstorems/web/BookRestController.java
@@ -3,6 +3,7 @@ package com.glb.training.bookstorems.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.glb.training.bookstorems.repository.BookRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
+@RefreshScope
 @Slf4j
 @RestController
 public class BookRestController {
