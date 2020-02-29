795cfaa9d56136299f449c9af0e7514d1b463e8c
Luis Cortes <lfcortes12@gmail.com>
2020-02-29 05:43:10 -0500

    updates properties to use ip address when the services register in eureka

--- book-store-gateway/src/main/java/com/glb/training/bookstoregateway/BookStoreGatewayApplication.java
+++ book-store-gateway/src/main/java/com/glb/training/bookstoregateway/BookStoreGatewayApplication.java
@@ -2,9 +2,10 @@ package com.glb.training.bookstoregateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-
+@EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
 public class BookStoreGatewayApplication {
