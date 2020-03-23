af255e8213812f14365dbb695cabccfd2f255cb6
Luis Cortes <lfcortes12@gmail.com>
2020-03-23 17:30:10 -0500

    enables hystrix dashboard on the gateway and also turbine

--- book-store-gateway/src/main/java/com/glb/training/bookstoregateway/BookStoreGatewayApplication.java
+++ book-store-gateway/src/main/java/com/glb/training/bookstoregateway/BookStoreGatewayApplication.java
@@ -3,14 +3,18 @@ package com.glb.training.bookstoregateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableHystrixDashboard
+@EnableTurbine
 @EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
 public class BookStoreGatewayApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(BookStoreGatewayApplication.class, args);
 	}
 
