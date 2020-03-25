3eae81bad8bb4f1f4cb9df313d4839cad3d2d2ab
Luis Cortes <lfcortes12@gmail.com>
2020-03-25 09:17:49 -0500

    config to support spring boot admin

--- book-store-registry-server/src/main/java/com/glb/training/bookstoreregistryserver/BookStoreRegistryServerApplication.java
+++ book-store-registry-server/src/main/java/com/glb/training/bookstoreregistryserver/BookStoreRegistryServerApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@EnableAdminServer
 @EnableEurekaServer
 @EnableConfigServer
 @SpringBootApplication
 public class BookStoreRegistryServerApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(BookStoreRegistryServerApplication.class, args);
 	}
 
